3854cbd70971706cc1f590c744c6db6b599fc60e
Shubham Pawar <65530086+Shubham51dot@users.noreply.github.com>
2020-11-20 22:58:20 +0530

    about me updated

--- index.js
+++ index.js
@@ -76,3 +76,49 @@ document.querySelector('.images').addEventListener('mouseleave',(e)=>{
    console.log(e);
 
 });
+
+// about me animations
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container3 = document.querySelector(".container3");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const profession = document.querySelector(".profession");
+const description = document.querySelector(".description");
+const sizes = document.querySelector(".sizes");
+
+//Moving Animation Event
+container3.addEventListener("mousemove", (e) => {
+
+  let xAxis = (1000  - e.pageX)/200 ;
+
+  let yAxis = (1000 - e.pageY)/200;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container3.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(100px)";
+  sneaker.style.transform = "translateZ(100px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(65px)";
+  profession.style.transform = "translateZ(120px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container3.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  profession.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+
+
+});
